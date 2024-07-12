c387b4d7db6ab4a079c8429d5dc7490ff1b9db02
Eduard <eduard.samoilenko@chargeafter.com>
2024-07-12 14:58:32 +0300

    Pretty code

--- packages/chargeafter-integration/src/ChargeafterPaymentMethod.tsx
+++ packages/chargeafter-integration/src/ChargeafterPaymentMethod.tsx
@@ -3,29 +3,29 @@ import React, { FunctionComponent, useMemo } from 'react';
 import { HostedPaymentComponent } from '@bigcommerce/checkout/hosted-payment-integration';
 import { TranslatedString } from '@bigcommerce/checkout/locale';
 import {
-    PaymentMethodProps,
-    PaymentMethodResolveId,
-    toResolvableComponent,
+  PaymentMethodProps,
+  PaymentMethodResolveId,
+  toResolvableComponent,
 } from '@bigcommerce/checkout/payment-integration-api';
 
 const ChargeafterPaymentMethod: FunctionComponent<PaymentMethodProps> = ({
-    checkoutService,
-    ...rest
+  checkoutService,
+  ...rest
 }) => {
-    const description = useMemo(() => <TranslatedString id="payment.chargeafter_body_text" />, []);
+  const description = useMemo(() => <TranslatedString id="payment.chargeafter_body_text" />, []);
 
-    return (
-        <HostedPaymentComponent
-            {...rest}
-            checkoutService={checkoutService}
-            deinitializePayment={checkoutService.deinitializePayment}
-            description={description}
-            initializePayment={checkoutService.initializePayment}
-        />
-    );
-}
+  return (
+    <HostedPaymentComponent
+      {...rest}
+      checkoutService={checkoutService}
+      deinitializePayment={checkoutService.deinitializePayment}
+      description={description}
+      initializePayment={checkoutService.initializePayment}
+    />
+  );
+};
 
 export default toResolvableComponent<PaymentMethodProps, PaymentMethodResolveId>(
-    ChargeafterPaymentMethod,
-    [{ id: 'chargeafter' }],
+  ChargeafterPaymentMethod,
+  [{ id: 'chargeafter' }],
 );
